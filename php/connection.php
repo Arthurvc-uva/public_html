c37feb59d73295aba85b50597b68acd8df660f12
Arthurvc-uva <Arthur.van.campenhout@student.uva.nl>
2023-01-30 12:07:24 +0100

    Nieuwe site

---
+++ php/connection.php
@@ -0,0 +1,9 @@
+<?php
+$servername = "localhost";
+$username = "hugoe";
+$password = "EDNZxAfEptklmtplrOmgeGquRAuvhNHw";
+$database = "Skilift website";
+
+$connection = new mysqli($servername, $username, $password, $database);
+
+?>
